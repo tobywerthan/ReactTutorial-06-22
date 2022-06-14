ec2b4abda25d19330edd77b50a5548b5f6535219
tobywerthan <tobywerthan1@gmail.com>
2022-06-14 15:26:20 -0500

    implemented a form with backend, posting and getting submitted data

--- src/Pages/NewMeetup.js
+++ src/Pages/NewMeetup.js
@@ -1,4 +1,24 @@
+import NewMeetupForm from "../Components/Layout/meetups/NewMeetupForm";
+import { useHistory } from "react-router-dom";
 function NewMeetupPage() {
-  return <div>New Meetup Page</div>;
+  const history = useHistory();
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-tutorial-06-22-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => [history.replace("/")]);
+  }
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 export default NewMeetupPage;
